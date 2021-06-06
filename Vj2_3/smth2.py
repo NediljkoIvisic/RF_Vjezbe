544275218071f43db1dca58e0c38b8f0f2299b63
NediljkoIvisic <nediljko.ivisic99@gmail.com>
2021-06-06 20:26:17 +0200

    Updates

--- Vj2_3/smth2.py
+++ Vj2_3/smth2.py
@@ -7,9 +7,10 @@ def calculate_checksum(filenames):
     for fn in filenames:
         with open(fn, 'rb') as inputfile:
             data = inputfile.read()
-            print("-----" + fn + "-----")
-            print(hashlib.sha1(data).hexdigest())
-            print(hashlib.md5(data).hexdigest())
+            print("******************" + fn + "******************")
+            print("  SHA 1: " + hashlib.sha1(data).hexdigest())
+            print("SHA 256: " + hashlib.sha256(data).hexdigest())
+            print("    MD5: " + hashlib.md5(data).hexdigest())
 
 if __name__ == "__main__":
     files = glob.glob('./*')
